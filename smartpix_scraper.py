534613f8b830779eaf0ee9cf2b31f8a0f73602ed
Aniket Dumbre <anktd23@gmail.com>
2023-02-01 14:48:44 +0530

    data extraction done

--- smartpix_scraper.py
+++ smartpix_scraper.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
-s = Service('C:/Users/Nitish/Desktop/chromedriver.exe')
+s = Service('"F:\chromedriver.exe"')
 
 driver = webdriver.Chrome(service = s)
 
